14c86350ada5335655235146565cc417b4501901
Paul Terwilliger <paul.terwil@gmail.com>
2022-01-14 12:03:38 -0500

    fixing workflows

--- tests/template_duplicate/test_numpy.py
+++ tests/template_duplicate/test_numpy.py
@@ -1,19 +1,24 @@
 import numpy as np
 
+
 def np_maximum(x, y):
     return np.maximum(x, y)
 
+
 def np_arange(x):
     return np.arange(x)
+
 
 def test_np_maximum():
     x = 3
     y = 5
     assert np_maximum(x, y) == y
 
+
 def test_np_arange():
     x = 5
     assert len(np_arange(5)) == x
+
 
 if __name__ == "__main__":
     pass
--- tests/template_duplicate/test_sanity.py
+++ tests/template_duplicate/test_sanity.py
@@ -1,27 +1,34 @@
 import pytest
 
+
 def upper(foo):
     return foo.upper()
 
+
 def increment(foo):
     return foo + 1
+
 
 def raises():
     raise Exception
 
 
 def test_base():
-    assert True == True
+    assert True is True
+
 
 def test_upper():
-    assert upper('foo') == 'FOO'
+    assert upper("foo") == "FOO"
+
 
 def test_increment():
     assert increment(10) == 11
 
+
 def test_raises():
     with pytest.raises(Exception):
         raises()
+
 
 if __name__ == "__main__":
     pass
