937613bba70deca43753735de44c4e65b6009d15
Paul Terwilliger <paul.terwil@gmail.com>
2022-08-27 17:00:00 -0400

    Update example.py

--- src/package_two/sanity/example.py
+++ src/package_two/sanity/example.py
@@ -9,6 +9,6 @@ def arange(number: int) -> np.typing.ArrayLike:
 def add(val_one: float, val_two: float) -> float:
     return val_one + val_two
 
-    
+
 def upper(text: str) -> str:
     return text.upper()
